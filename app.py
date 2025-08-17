0986a407bc1c510794eeca77bfc86c42550b6599
AMAShyp <hypermarketamas@gmail.com>
2025-08-17 13:27:53 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.set_page_config(page_title="Inventory Management System")
+st.title("Welcome to the Inventory Management System")
+st.write("Select 'Selling Area' from the sidebar to begin.")
